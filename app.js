df7b0b829cc413063914037097e6f0c85cd28d2a
Gibby <rgibson@bulldogsolutions.ca>
2021-10-11 21:10:11 -0400

    output webpage

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = (profileDataArr) => {
-
-    profileDataArr.forEach(poo => console.log(poo));
-};
+// const printProfileData = (profileDataArr) => {
+//     profileDataArr.forEach(poo => console.log(poo));
+// };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
